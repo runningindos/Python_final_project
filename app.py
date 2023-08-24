b5385153bf09d40014ad3477e2f9bed206693fab
runningindos <56303584+runningindos@users.noreply.github.com>
2023-08-24 21:31:14 +0100

    Added paths to the app.py page

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/message_board.html")
+def message_board():
+    return render_template("message_board.html")
+
+@app.route("/photos")
+def photos():
+    return render_template("photos.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
